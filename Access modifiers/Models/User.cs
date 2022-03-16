facb9625233bc405fc9c7eb3a589e0f4723d5f3e
elgun qocayev <elgun8195.tu741rmps@code.edu.az>
2022-03-16 10:51:33 +0400

    Uncreated task((

---
+++ Access modifiers/Models/User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Access_modifiers.Models
+{
+    internal class User
+    {
+
+        public string username;
+        private int age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("0 ola bilmez");
+                }
+            }
+        }
+        public string password
+        {
+            get
+            {
+                if (password == "")  // || 
+                {
+                    Console.WriteLine("bosh ola bilmez");
+                }
+
+                if (password.Length < 8)
+                {
+                    Console.WriteLine("8-den kicik ola bilmez");
+                }
+                return password;
+            }
+            set
+            {
+                
+                //if ()
+                //{
+
+                //}
+
+            }
+
+        }
+
+
+        public User(string username, string password,int age)
+        {
+            username = username;
+            age=age;
+            password = password;
+        }
+    }
+}
+
---
+++ Access modifiers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Access_modifiers.Models;
+
+namespace Access_modifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User el = new User("salam", "gf",0);
+            Console.WriteLine(el);
+           
+        }
+    }
+}
